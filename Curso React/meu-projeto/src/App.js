bea68afddad8f278c04c224e3526346d3cdc2244
Igor Gassen <igorhgassen@gmail.com>
2023-08-23 16:00:03 -0300

    Projetos adicionados

---
+++ Curso React/meu-projeto/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Empresa from "./pages/Empresa";
+import Contato from "./pages/Contato";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
+function App() {
+
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/empresa" element={<Empresa />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  )
+}
+
+export default App;
